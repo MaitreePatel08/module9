9cea160ca666c220ee36c85f08063f0689194d4b
MaitreePatel08 <maitreepatel012@gmail.com>
2023-09-09 10:59:18 -0400

    Initializing app

--- index.js
+++ index.js
@@ -76,8 +76,11 @@ function writeToFile(fileName, data) {
     return fs.writeFileSync(path.join(process.cwd(), fileName), data);
   }
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+// Initializing app
+function init() {
+    inquirer.prompt(questions).then((responses) => {
+      console.log("Creating Professional README.md File...");
+      writeToFile("./dist/README.md", generateMarkdown({ ...responses }));
+    });
+  }
+  init();
